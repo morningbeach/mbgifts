48034d9831c8faf790567a75db361af1d0bd180a
morningbeach <morningbeachtw@gmail.com>
2025-11-12 04:04:07 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: { allowedOrigins: ['*'] } },
+  output: 'export',
+  experimental: {
+    runtime: 'edge',
+    serverActions: { allowedOrigins: ['*'] },
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      async_hooks: false, // 防止 Cloudflare Pages build error
+    };
+    return config;
+  },
 };
 export default nextConfig;
